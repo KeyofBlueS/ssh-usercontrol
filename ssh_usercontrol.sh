71d018d8cf0040b98f7e27befbc67b5e583f2de6
KeyofBlueS <45725677+KeyofBlueS@users.noreply.github.com>
2019-02-26 06:10:11 +0100

    Update ssh_usercontrol.sh

--- ssh_usercontrol.sh
+++ ssh_usercontrol.sh
@@ -1,9 +1,59 @@
 #!/bin/bash
 
-# Version:    1.0.0
+# Version:    1.0.2
 # Author:     KeyofBlueS
 # Repository: https://github.com/KeyofBlueS/ssh-usercontrol
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
+
+if curl -s github.com > /dev/null; then
+	SCRIPT_LINK="https://raw.githubusercontent.com/KeyofBlueS/ssh-usercontrol/master/ssh_usercontrol.sh"
+	UPSTREAM_VERSION="$(timeout -s SIGTERM 15 curl -L "$SCRIPT_LINK" 2> /dev/null | grep "# Version:" | head -n 1)"
+	LOCAL_VERSION="$(cat "${0}" | grep "# Version:" | head -n 1)"
+	REPOSITORY_LINK="$(cat "${0}" | grep "# Repository:" | head -n 1)"
+	if echo "$LOCAL_VERSION" | grep -q "$UPSTREAM_VERSION"; then
+		echo -n
+	else
+		echo -e "\e[1;33m-----------------------------------------------------------------------------------	
+## ATTENZIONE: questo script non risulta aggiornato alla versione upstream, visita:
+\e[1;32m$REPOSITORY_LINK
+
+\e[1;33m$LOCAL_VERSION (locale)
+\e[1;32m$UPSTREAM_VERSION (upstream)
+\e[1;33m-----------------------------------------------------------------------------------
+
+\e[1;35mPremi invio per aggiornare questo script o attendi 10 secondi per andare avanti normalmente
+\e[1;31m## ATTENZIONE: eventuali modifiche effettuate a questo script verranno perse!!!
+\e[0m
+"
+		if read -t 10 _e; then
+			echo -e "\e[1;34m	Aggiorno questo script...\e[0m"
+			if [[ -L "${0}" ]]; then
+				scriptpath="$(readlink -f "${0}")"
+			else
+				scriptpath="${0}"
+			fi
+			if [ -z "${scriptfolder}" ]; then
+				scriptfolder="${scriptpath}"
+				if ! [[ "${scriptpath}" =~ ^/.*$ ]]; then
+					if ! [[ "${scriptpath}" =~ ^.*/.*$ ]]; then
+					scriptfolder="./"
+					fi
+				fi
+				scriptfolder="${scriptfolder%/*}/"
+				scriptname="${scriptpath##*/}"
+			fi
+			if timeout -s SIGTERM 15 curl -s -o /tmp/"${scriptname}" "$SCRIPT_LINK"; then
+				sudo mv /tmp/flash_update.sh "${scriptfolder}"
+				sudo chown root:root "${scriptfolder}${scriptname}" > /dev/null 2>&1
+				sudo chmod 755 "${scriptfolder}${scriptname}" > /dev/null 2>&1
+				sudo chmod +x "${scriptfolder}${scriptname}" > /dev/null 2>&1
+				echo -e "\e[1;34m	Fatto!
+\e[0m"
+				exec "${scriptfolder}${scriptname}"
+			fi
+		fi
+	fi
+fi
 
 for name in yad
 do
@@ -22,11 +72,10 @@ done
 pkill -15 -f "yad --notification --text=Utenti remoti connessi via ssh"
 pkill -15 -f "yad --title=Utenti ssh connessi"
 
-echo $@ | grep -Poq '\d+'
-if [ $? = 0 ]; then
-TIME="$(echo $@ | grep -Po '\d+')"
+if echo $@ | grep -Poq '\d+'; then
+	TIME="$(echo $@ | grep -Po '\d+')"
 else
-TIME=10
+	TIME=10
 fi
 
 ssh_userconnection(){
@@ -36,9 +85,8 @@ echo -e "\e[1;34m
 while true
 do
 CURRENTPTS="$(who | grep "pts")"
-echo $CURRENTPTS | grep -q "pts"
-if [ $? = 0 ]; then
-		echo -e "\e[1;31m
+if echo $CURRENTPTS | grep -q "pts"; then
+	echo -e "\e[1;31m
 ## UTENTE REMOTO CONNESSO ##
 	\e[0m"
 	date
@@ -56,12 +104,12 @@ Imposto lista utenti
 			yad --notification --text="Utenti remoti connessi via ssh" --command="yad --title='Utenti ssh connessi' --center --text='`who | grep pts`' --button=gtk-ok:0" &
 		fi
 	else
-			echo -e "\e[1;31m
+		echo -e "\e[1;31m
 Aggiorno lista utenti
 			\e[0m"
-			pkill -15 -f "yad --notification --text=Utenti remoti connessi via ssh"
-			pkill -15 -f "yad --title=Utenti ssh connessi"
-			yad --notification --text="Utenti remoti connessi via ssh" --command="yad --title='Utenti ssh connessi' --center --text='`who | grep pts`' --button=gtk-ok:0" &
+		pkill -15 -f "yad --notification --text=Utenti remoti connessi via ssh"
+		pkill -15 -f "yad --title=Utenti ssh connessi"
+		yad --notification --text="Utenti remoti connessi via ssh" --command="yad --title='Utenti ssh connessi' --center --text='`who | grep pts`' --button=gtk-ok:0" &
 	fi
 	STOREDPTS="$(who | grep "pts")"
 	echo -e "\e[1;31m
@@ -90,7 +138,7 @@ givemehelp(){
 echo "
 # ssh-usercontrol
 
-# Version:    1.0.0
+# Version:    1.0.2
 # Author:     KeyofBlueS
 # Repository: https://github.com/KeyofBlueS/ssh-usercontrol
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
@@ -115,12 +163,10 @@ $ ssh-usercontrol
 exit 0
 }
 
-if [ "$1" = "" ]
-then
-   ssh_userconnection
-elif [ "$1" = "--help" ]
-then
-   givemehelp
+if [ "$1" = "" ]; then
+	ssh_userconnection
+elif [ "$1" = "--help" ]; then
+	givemehelp
 else
-   ssh_userconnection
+	ssh_userconnection
 fi
